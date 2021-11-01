df97e4053654c684c9dfac827f05c31f3c5f5abe
GregoirePelegrin <46794177+GregoirePelegrin@users.noreply.github.com>
2021-11-01 14:02:28 +0100

    Removal of batch_size

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ DATA_TRAIN_PATH = '../data/train.csv'
 y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 
 # Define the parameters of the algorithm.
-batch_size = 1
 max_iters = 5000
 gamma = 0.7
 
@@ -18,10 +17,10 @@ std_tx = standardize(tX)
 std_y = np.asmatrix(y).T
 std_tx = np.c_[np.ones((std_tx.shape[0], 1)), std_tx]
 
-def stochastic_gradient_descent(y, tx, batch_size, max_iters, gamma):
+def stochastic_gradient_descent(y, tx, max_iters, gamma):
     w_initial = np.zeros((tx.shape[1], 1))
     start_time = datetime.datetime.now()
-    w, loss = least_squares_SGD(y, tx, w_initial, batch_size, max_iters, gamma)
+    w, loss = least_squares_SGD(y, tx, w_initial, max_iters, gamma)
     end_time = datetime.datetime.now()
 
     # Print result
@@ -30,7 +29,7 @@ def stochastic_gradient_descent(y, tx, batch_size, max_iters, gamma):
 
     return w, loss
 
-weights, loss = stochastic_gradient_descent(std_y, std_tx, batch_size, max_iters, gamma)
+weights, loss = stochastic_gradient_descent(std_y, std_tx, max_iters, gamma)
 
 DATA_TEST_PATH = '../data/test.csv'
 _, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
